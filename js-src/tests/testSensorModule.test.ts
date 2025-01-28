6365dfa36493b920ff59602db677a9ec6cef3fbc
william-ketryx <williamb@ketryx.com>
2025-01-28 11:34:50 -0500

    Update testSensorModule.test.ts

--- js-src/tests/testSensorModule.test.ts
+++ js-src/tests/testSensorModule.test.ts
@@ -1,4 +1,4 @@
-describe('Javascript automated test for sensor module @tests:KXITM4XS49623PY9F2BFRA2S67Y2JSJ', () => {
+describe('Javascript automated test for sensor module @tests:KXITM3VXJ4810T689689N9MS41EB976', () => {
   it('sensor module functions correctly', () => {
     // This would test the sensor module.
     expect(1 + 2).toBe(3);
